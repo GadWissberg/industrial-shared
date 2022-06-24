e85b7b68991761d26de44a1571cd05717d8137fe
Gad Wissberg <gadw17@gmail.com>
2022-06-25 00:54:32 +0300

    Switching to industrial.

--- core/src/com/gadarts/industrial/shared/model/env/ThingsDefinitions.java
+++ core/src/com/gadarts/industrial/shared/model/env/ThingsDefinitions.java
@@ -3,7 +3,6 @@ package com.gadarts.industrial.shared.model.env;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.math.Vector3;
 import com.gadarts.industrial.shared.assets.definitions.ModelDefinition;
-import com.gadarts.industrial.shared.model.ModelElementDefinition;
 import com.gadarts.industrial.shared.model.RelativeBillboard;
 import com.gadarts.industrial.shared.model.characters.Direction;
 import com.gadarts.industrial.shared.model.map.MapNodesTypes;
@@ -22,11 +21,11 @@ public enum ThingsDefinitions implements EnvironmentObjectDefinition {
 	WALL_SUPPORTER_2(Assets.Models.WALL_SUPPORTER_2, 1, 1, "Wall Supporter #2", new Vector3(-0.5F, 0F, 0.5F)),
 	WALL_SUPPORTER_3(Assets.Models.WALL_SUPPORTER_3, 1, 1, "Wall Supporter #3", new Vector3(-0.5F, 0F, 0.5F)),
 	WALL_SUPPORTER_HIGH(Assets.Models.WALL_SUPPORTER_HIGH, 1, 1, "High Wall Supporter", new Vector3(-0.5F, 0F, 0.5F)),
-	COMPUTER_WAGON(Assets.Models.COMPUTER_WAGON, 1, 1, "Computer Wagon"),
-	CRATE_BIG_0(Assets.Models.CRATE_BIG_0, 1, 1, "Big Crate #1"),
-	CRATE_BIG_1(Assets.Models.CRATE_BIG_1, 1, 1, "Big Crate #2"),
-	CRATE_BIG_2(Assets.Models.CRATE_BIG_2, 1, 1, "Big Crate #3"),
-	CRATE_SMALL(Assets.Models.CRATE_SMALL, 1, 1, "Small Crate");
+	COMPUTER_WAGON(Assets.Models.COMPUTER_WAGON, 1, 1, "Computer Wagon", MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
+	CRATE_BIG_0(Assets.Models.CRATE_BIG_0, 1, 1, "Big Crate #1", MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
+	CRATE_BIG_1(Assets.Models.CRATE_BIG_1, 1, 1, "Big Crate #2", MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
+	CRATE_BIG_2(Assets.Models.CRATE_BIG_2, 1, 1, "Big Crate #3", MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
+	CRATE_SMALL(Assets.Models.CRATE_SMALL, 1, 1, "Small Crate", MapNodesTypes.OBSTACLE_KEY_DIAGONAL_ALLOWED);
 
 	@Getter(AccessLevel.NONE)
 	private final Vector3 offset;
@@ -45,7 +44,8 @@ public enum ThingsDefinitions implements EnvironmentObjectDefinition {
 	ThingsDefinitions(Assets.Models model,
 					  int width,
 					  int depth,
-					  String displayName) {
+					  String displayName,
+					  MapNodesTypes nodeType) {
 		this(
 				model,
 				width,
@@ -53,7 +53,7 @@ public enum ThingsDefinitions implements EnvironmentObjectDefinition {
 				displayName,
 				true,
 				Vector3.Zero,
-				MapNodesTypes.OBSTACLE_KEY_DIAGONAL_ALLOWED,
+				nodeType,
 				null,
 				null);
 	}
