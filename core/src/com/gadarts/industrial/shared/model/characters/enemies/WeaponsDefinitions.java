6bccdde673db0eccb30d124483603c6eea39ca3e
Gad Wissberg <gadw17@gmail.com>
2022-07-11 22:48:29 +0300

    Switching to industrial.

--- core/src/com/gadarts/industrial/shared/model/characters/enemies/WeaponsDefinitions.java
+++ core/src/com/gadarts/industrial/shared/model/characters/enemies/WeaponsDefinitions.java
@@ -13,21 +13,21 @@ public enum WeaponsDefinitions {
 			Assets.ParticleEffects.ENERGY_BALL_EXPLOSION,
 			Assets.Sounds.ATTACK_ENERGY_BALL,
 			Assets.Sounds.SMALL_EXP,
-			5, 7, false),
+			5, 7, 4),
 	COLT(
 			0.1F,
 			2,
 			Assets.ParticleEffects.ENERGY_BALL_EXPLOSION,
 			Assets.Sounds.ATTACK_ENERGY_BALL,
 			Assets.Sounds.SMALL_EXP,
-			5, 7, false),
+			5, 7),
 	HAMMER(
 			0.1F,
 			2,
 			Assets.ParticleEffects.ENERGY_BALL_EXPLOSION,
 			Assets.Sounds.ATTACK_ENERGY_BALL,
 			Assets.Sounds.SMALL_EXP,
-			5, 7, false);
+			1, 7, false);
 
 	private final float frameDuration;
 	private final Integer damage;
@@ -37,5 +37,55 @@ public enum WeaponsDefinitions {
 	private final int minNumberOfBullets;
 	private final int maxNumberOfBullets;
 	private final boolean melee;
+	private final int engineConsumption;
 
+	WeaponsDefinitions(float frameDuration,
+					   int damage,
+					   Assets.ParticleEffects particleEffect,
+					   Assets.Sounds engageSound,
+					   Assets.Sounds impactSound,
+					   int minNumberOfBullets, int maxNumberOfBullets,
+					   int engineConsumption) {
+		this(frameDuration,
+				damage,
+				particleEffect,
+				engageSound,
+				impactSound,
+				minNumberOfBullets, maxNumberOfBullets,
+				false,
+				engineConsumption);
+	}
+
+	WeaponsDefinitions(float frameDuration,
+					   int damage,
+					   Assets.ParticleEffects particleEffect,
+					   Assets.Sounds engageSound,
+					   Assets.Sounds impactSound,
+					   int minNumberOfBullets, int maxNumberOfBullets) {
+		this(frameDuration,
+				damage,
+				particleEffect,
+				engageSound,
+				impactSound,
+				minNumberOfBullets, maxNumberOfBullets,
+				false,
+				0);
+	}
+
+	WeaponsDefinitions(float frameDuration,
+					   int damage,
+					   Assets.ParticleEffects particleEffect,
+					   Assets.Sounds engageSound,
+					   Assets.Sounds impactSound,
+					   int minNumberOfBullets, int maxNumberOfBullets,
+					   boolean melee) {
+		this(frameDuration,
+				damage,
+				particleEffect,
+				engageSound,
+				impactSound,
+				minNumberOfBullets, maxNumberOfBullets,
+				melee,
+				0);
+	}
 }
