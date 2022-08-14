b984fc195b7cea60f5d6171bb2e361e8156c52e4
Gad Wissberg <gadw17@gmail.com>
2022-08-14 23:39:45 +0300

    Switching to industrial.

--- core/src/com/gadarts/industrial/shared/model/characters/SpriteType.java
+++ core/src/com/gadarts/industrial/shared/model/characters/SpriteType.java
@@ -8,14 +8,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SpriteType {
-	IDLE(0.3f, Animation.PlayMode.LOOP_PINGPONG),
+	IDLE(0F, Animation.PlayMode.LOOP_PINGPONG, 2),
 	RUN(0.06f),
 	ATTACK_PRIMARY(0.06f, Animation.PlayMode.NORMAL),
 	PAIN(0.1F, Animation.PlayMode.NORMAL, false, true),
 	PICKUP(0.1f, Animation.PlayMode.NORMAL, false, true),
-	LIGHT_DEATH_1(0.06f, Animation.PlayMode.NORMAL, true, false, true),
-	LIGHT_DEATH_2(0.06f, Animation.PlayMode.NORMAL, true, false, true),
-	LIGHT_DEATH_3(0.06f, Animation.PlayMode.NORMAL, true, false, true);
+	LIGHT_DEATH(0.06f, Animation.PlayMode.NORMAL, true, false, true, 3);
 
 	private final float animationDuration;
 	private final Animation.PlayMode playMode;
@@ -23,43 +21,37 @@ public enum SpriteType {
 	private final boolean addReverse;
 	private final boolean death;
 	private final String regionName;
+	private final int variations;
 
 	SpriteType(final float animationDuration) {
-		this(animationDuration, Animation.PlayMode.LOOP);
+		this(animationDuration, Animation.PlayMode.LOOP, 1);
 	}
 
-	SpriteType(final float animationDuration, final Animation.PlayMode playMode) {
-		this(animationDuration, playMode, false, false, false, null);
+	SpriteType(float animationDuration, Animation.PlayMode playMode) {
+		this(animationDuration, playMode, false, false, false, null, 1);
+	}
+
+	SpriteType(final float animationDuration, final Animation.PlayMode playMode, int variations) {
+		this(animationDuration, playMode, false, false, false, null, variations);
 	}
 
 	SpriteType(final float animationDuration,
 			   final Animation.PlayMode playMode,
 			   final boolean singleAnimation,
 			   final boolean addReverse) {
-		this(animationDuration, playMode, singleAnimation, addReverse, false, null);
-	}
-
-
-	SpriteType(float animationDuration, Animation.PlayMode playMode, String regionName) {
-		this(animationDuration, playMode, false, false, false, regionName);
+		this(animationDuration, playMode, singleAnimation, addReverse, false, null, 1);
 	}
 
 	SpriteType(float animationDuration,
 			   Animation.PlayMode playMode,
 			   boolean singleAnimation,
 			   boolean addReverse,
-			   boolean death) {
-		this(animationDuration, playMode, singleAnimation, addReverse, death, null);
+			   boolean death,
+			   int variations) {
+		this(animationDuration, playMode, singleAnimation, addReverse, death, null, variations);
 	}
 
-	public static SpriteType randomLightDeath( ) {
-		int random = MathUtils.random(3);
-		if (random == 0) {
-			return LIGHT_DEATH_1;
-		} else if (random == 1) {
-			return LIGHT_DEATH_2;
-		} else {
-			return LIGHT_DEATH_3;
-		}
+	public static int randomLightDeath( ) {
+		return MathUtils.random(3);
 	}
 }
