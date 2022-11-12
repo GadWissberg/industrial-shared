70c3403bc2408e785cd060a8a5decd8ec96b1239
Gad Wissberg <gadw17@gmail.com>
2022-11-12 22:18:58 +0200

    Working on demo map.

--- core/src/com/gadarts/industrial/shared/model/env/LightConstants.java
+++ core/src/com/gadarts/industrial/shared/model/env/LightConstants.java
@@ -1,8 +1,8 @@
 package com.gadarts.industrial.shared.model.env;
 
 final public class LightConstants {
-	public static final float DEFAULT_LIGHT_HEIGHT = 1.9F;
+	public static final float DEFAULT_LIGHT_HEIGHT = 2F;
 	public static final float DEFAULT_LIGHT_RADIUS = 4;
-	public static final float DEFAULT_LIGHT_INTENSITY = 0.5F;
+	public static final float DEFAULT_LIGHT_INTENSITY = 1F;
 
 }
